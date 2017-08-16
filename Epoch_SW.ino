4dd683e74d62e3dc5908fe83620d5f60ba47935b
winneymj <winneymj@gmail.com>
2017-08-15 19:37:19 -0500

    Implement deep sleep and interrupts

--- Epoch_SW.ino
+++ Epoch_SW.ino
@@ -1,9 +1,11 @@
+#include <Time.h>         //http://www.arduino.cc/playground/Code/Time  
+#include <TimeLib.h>         //http://www.arduino.cc/playground/Code/Time  
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SharpMem.h>
 #include <Watch_Menu.h>
-#include <Wire.h>
-#include <RTCx.h>
 #include <Button.h> // https://github.com/JChristensen/Button
+#include <DS3232RTC.h>    //http://github.com/JChristensen/DS3232RTC
 
 #include "icons.h"
 #include "resources.h"
@@ -12,33 +14,54 @@
 #define MOSI 18
 #define SS 23
 #define EXTMODE 28
-#define LED 6
-#define RTC_INT 9
 
+#define LED 6
 #define MBUT 4
 #define UBUT 14
 #define DBUT 22
-Button buttonMid(MBUT, true, true, 20);
-Button buttonUp(UBUT, true, true, 20);
-Button buttonDown(DBUT, true, true, 20);
+#define RTC_INT 9
+
+#define EVERY_SECOND
+//#define EVERY_MINUTE
 
 #define BLACK 0
 #define WHITE 1
 #define INVERSE 2
 
 
+Button buttonMid(MBUT, true, true, 20);
+Button buttonUp(UBUT, true, true, 20);
+Button buttonDown(DBUT, true, true, 20);
+
 Adafruit_SharpMem display(SCK, MOSI, SS);
 WatchMenu menu(display);
+volatile boolean buttonRead = false; //variables in ISR need to be volatile
+volatile boolean buttonFired = false; //variables in ISR need to be volatile
+volatile boolean rtcRead = false; //variables in ISR need to be volatile
+volatile boolean rtcFired = false; //variables in ISR need to be volatile
 
-RTCx DS3231M(RTCx::DS3231MAddress, RTCx::DS3231M);
-RTCx MCP7941(RTCx::MCP7941xAddress, RTCx::MCP7941x);
+DS3232RTC MyDS3232;
+//RTCx MCP7941(RTCx::MCP7941xAddress, RTCx::MCP7941x);
 
 byte activeTime = 15; //how many sec until entering standby
 unsigned long standbyTimer;
 boolean active = false;
 
 static bool toggle = false;
-  
+
+void enableInterrupts()
+{
+  // Enable EIC
+  EIC->CTRL.bit.ENABLE = 1;
+  while (EIC->STATUS.bit.SYNCBUSY == 1) { }
+}
+
+void disableInterrupts()
+{
+  // Enable EIC
+  EIC->CTRL.bit.ENABLE = 0;
+  while (EIC->STATUS.bit.SYNCBUSY == 1) { }
+}
 
 void mainFunc()
 {
@@ -85,87 +108,133 @@ void mainFunc()
 ////  setupButtons();
 //}
 
-//Wake from Middle Button
-void wake()
+void RTC_int() //ISR for RTC interrupt overy minute
 {
-  noInterrupts();  // Disable interrupts
-  standbyTimer = millis()+activeTime*1000; // reset Standby Timer
-  toggle = !toggle;
-//  digitalWrite(EXTMODE, LOW); // switch VCOM to software
-  interrupts();  //Enable interrupts
+  disableInterrupts();
+  rtcRead = !rtcRead;
+  rtcFired = true;
+  enableInterrupts();
+}
+
+void myISR() //ISR for button presses
+{
+  disableInterrupts();  // This must exist, especially with the LOW trigger.  For some reason
+  // when a LOW is interrupt trigger is received it continues to fire and locks the processor.
+  // I am using a LOW trigger as I am planning to put the processor into deep sleep where the clocks
+  // are disabled. Other triggers (FALLING, RISING etc) need the clock running to be triggered.
+  
+  buttonRead = !buttonRead;
+  buttonFired = true;
+  enableInterrupts();
+}
+
+void initializePins()
+{
+  // Set pullups for all the interrupts
+  pinMode(MBUT, INPUT_PULLUP);
+  pinMode(UBUT, INPUT_PULLUP);
+  pinMode(DBUT, INPUT_PULLUP);
+  pinMode(RTC_INT, INPUT_PULLUP); // RTC Interrupt
+
+  // Set LED pin to output.
+  pinMode(LED, OUTPUT);
+
+  // RTC square wave VCOM at 1Hz  
+  pinMode(EXTMODE, OUTPUT); //VCOM Mode (h=ext l=sw)
+//  digitalWrite(EXTMODE, HIGH); // switch VCOM to external
+
+}
+
+void initializeRTC()
+{
+  // Disable the RTC interrupts for the moment.
+  MyDS3232.alarmInterrupt(ALARM_1, false);
+  MyDS3232.alarmInterrupt(ALARM_2, false);
+
+  // Attach button and RTC interrupt routine to the pins.
+  attachInterrupt(digitalPinToInterrupt(MBUT), myISR, LOW); // when button B is pressed display 
+  attachInterrupt(digitalPinToInterrupt(UBUT), myISR, LOW); // when button C is pressed display 
+  attachInterrupt(digitalPinToInterrupt(DBUT), myISR, LOW); // when button C is pressed display battery status
+  attachInterrupt(digitalPinToInterrupt(RTC_INT), RTC_int, LOW); // RTC Interrupt
+
+  // Set RTC to interrupt every second for now just to make sure it works.
+  // Will finally set to one minute.
+#ifdef EVERY_SECOND
+  MyDS3232.setAlarm(ALM1_EVERY_SECOND, 0, 0, 0);
+  MyDS3232.alarmInterrupt(ALARM_1, true);
+#endif
+#ifdef EVERY_MINUTE
+  MyDS3232.setAlarm(ALM2_EVERY_MINUTE, 0, 0, 0);
+  MyDS3232.alarmInterrupt(ALARM_2, true);
+#endif
+
+  // Ensure the oscillator is running.
+//  MCP7941.startClock();
+//  Serial.println("MCP7941.startClock()");
+
+//  MCP7941.setSQW(RTCx::freq1Hz);
+//  Serial.println("MCP7941.setSQW()");
+}
+
+void sleepProcessor()
+{
+  //wait-for-interrupt has no effect unless the sleep bit is set in the
+  //System Control Register (SCR)(see setup, in the attachInterrupt area)
+  //if the sleep bit is set, we wait after this instruction for an interrupt 
+//  PM->SLEEP.reg |= PM_SLEEP_IDLE_CPU;
+  PM->SLEEP.reg |= PM_SLEEP_IDLE_APB;
+
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;  // set to deep sleep (bit-1)
+
+  // Clear the alarm interrupt in the RTC, else we will never wake up from sleep.
+  // Very strange happening that only exhibits self when interrupt trigger is LOW.
+#ifdef EVERY_SECOND
+  uint8_t stat = MyDS3232.alarm(ALARM_1); 
+//  Serial.println("sleep SECOND");
+#endif
+#ifdef EVERY_MINUTE
+  uint8_t stat = MyDS3232.alarm(ALARM_2); 
+//  Serial.println("sleep MINUTE");
+#endif
+  
+//  Serial.println("sleep");
+//  delay(500);
+//  Serial.end();
+  
+  __WFI(); // Now wait for interrupt.
 }
 
 void setup()
 {
   Serial.begin(9600);
-  while (!Serial); 
+//  while (!Serial); 
 
-  Serial.println("before Wire.begin()");
   Wire.begin();
-  Serial.println("after Wire.begin()");
+
+  // Setup the pins for buttons etc.
+  initializePins();
+
+  // Initialize the RTC (DS3232 and MCP7941)
+  initializeRTC();
   
-  Serial.print("MCP7941 Address=");
-  Serial.println(MCP7941.getAddress(), HEX);
-
-  // Ensure the oscillator is running.
-  MCP7941.startClock();
-  Serial.println("MCP7941.startClock()");
-
-  MCP7941.setSQW(RTCx::freq1Hz);
-  Serial.println("MCP7941.setSQW()");
-
-  // Clear out RTC status bits
-  DS3231M.clearStatusBit(A2F | EN32KHZ | BSY | A2F | A1F);
-
-  // Ensure the oscillator is running.
-  DS3231M.startClock();
-  Serial.println("DS3231M.startClock()");
-
-//  DS3231M.setSQW(RTCx::freq1Hz);
-//  Serial.println("DS3231M.setSQW()");
-
-  // Clear any existing alarms
-  DS3231M.clearAlarm(RTCx::ALARM1);
-  DS3231M.clearAlarm(RTCx::ALARM2);
-
-  // Enable alarm 2 
-  DS3231M.enableAlarm(RTCx::ALARM2, true);
-  Serial.println("DS3231M.enableAlarm()");
-
-  DS3231M.setAlarm(RTCx::once_per_min);
-
-  pinMode(EXTMODE, OUTPUT); //VCOM Mode (h=ext l=sw)
-  digitalWrite(EXTMODE, HIGH); // switch VCOM to external
 
   // start & clear the display
-  display.begin();
-  display.clearDisplay();
+//  display.begin();
+//  display.clearDisplay();
 
   // Initialise the display
 //  initializeMenu();
 
-  // Button setups
-  pinMode(MBUT, INPUT_PULLUP); // Middle Button Pullup
-  pinMode(UBUT, INPUT_PULLUP); // Up Button Pullup
-  pinMode(DBUT, INPUT_PULLUP); // Down Button Pullup
-  pinMode(RTC_INT, INPUT_PULLUP); // RTC Interrupt
-
-  pinMode(EXTMODE, OUTPUT);
-  digitalWrite(EXTMODE, LOW); // switch VCOM to software
-
-    // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED, OUTPUT);
-
-  attachInterrupt(MBUT, wake, FALLING); // Middle Button Interrupt
-  attachInterrupt(UBUT, wake, FALLING); // Middle Button Interrupt
-  attachInterrupt(DBUT, wake, FALLING); // Middle Button Interrupt
-  attachInterrupt(RTC_INT, wake, FALLING); // RTC Interrupt
+//  pinMode(EXTMODE, OUTPUT);
+//  digitalWrite(EXTMODE, LOW); // switch VCOM to software
 
   //standbyTimer = millis()+activeTime*1000;
 
   buttonMid.read();
   buttonUp.read();
   buttonDown.read();
+
+//  Serial.println("setup: exit");
 }
 
 // Font data for Arial 48pt
@@ -194,6 +263,12 @@ int displayColon(uint8_t x, uint8_t y)
 
 void loop(void) 
 {
+  // Sleep and wait for interrupt from buttons or RTC
+  sleepProcessor();
+  
+  digitalWrite(LED, rtcRead ? HIGH : LOW);
+
+
 //  active = (millis()<=standbyTimer); //check if active
 //  buttonMid.read(); //read Button
 //  Serial.print("active=");
@@ -216,51 +291,41 @@ void loop(void)
 //  }
 //  else
 //  {
-     delay(500);
+//     delay(500);
 //  }
   
   buttonMid.read();
   if (buttonMid.wasPressed())
   {
-    Serial.println("Mid wasPressed");
+//    Serial.println("Mid wasPressed");
   }
 
   buttonUp.read();
   if (buttonUp.wasPressed())
   {
-    Serial.println("Up wasPressed");
+//    Serial.println("Up wasPressed");
   }
 
   buttonDown.read();
   if (buttonDown.wasPressed())
   {
-    Serial.println("Down wasPressed");
+//    Serial.println("Down wasPressed");
   }
 
-  pinMode(EXTMODE, OUTPUT); //VCOM Mode (h=ext l=sw)
-  digitalWrite(EXTMODE, HIGH); // switch VCOM to external
+//  pinMode(EXTMODE, OUTPUT); //VCOM Mode (h=ext l=sw)
+//  digitalWrite(EXTMODE, HIGH); // switch VCOM to external
   
 //  Serial.println("Looping");
-  int xpos = 5;
-  xpos += displayChar(xpos, 10, 0);
-  xpos += displayChar(xpos, 10, 4);
-  xpos += displayColon(xpos, 15);
-  xpos += displayChar(xpos, 10, 4);
-  xpos += displayChar(xpos, 10, 4);
+//  int xpos = 5;
+//  xpos += displayChar(xpos, 10, 0);
+//  xpos += displayChar(xpos, 10, 4);
+//  xpos += displayColon(xpos, 15);
+//  xpos += displayChar(xpos, 10, 4);
+//  xpos += displayChar(xpos, 10, 4);
 
-  digitalWrite(LED, (toggle == true) ? HIGH : LOW);
+//  digitalWrite(LED, (toggle == true) ? HIGH : LOW);
 
-  display.refresh();
+//  display.refresh();
 
-  uint8_t stat = DS3231M.getStatus();
-  Serial.print("DS3231M.getStatus():");
-  Serial.println(stat, HEX);
-
-  if (stat & A2F) // See if alarm2 is triggered
-  {
-    Serial.print("Alarm2 triggered at now:");
-    Serial.println(millis());
-    DS3231M.clearStatusBit(A2F);
-  }
 }
 
